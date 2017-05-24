9b9b6819f4142a60c0d2afcb544fd790190919c0
Cherié Warren <CheriePhi@msn.com>
2017-05-23 20:31:21 -0700

    Added KeyFiles.

---
+++ Diary/Persistence/KeyFile.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+namespace Diary
+{
+    public class KeyFile
+    {
+        /// <summary>
+        /// File to retrieve and save keys.
+        /// </summary>
+        private String mPathname;
+        private Dictionary<ObjectId, OffsetAndLength> mKeys;
+
+        /// <summary>
+        /// Creates a key file.
+        /// </summary>
+        /// <param name="folder"></param>
+        /// <param name="filename"></param>
+        public KeyFile(String folder, String filename)
+        {
+            mPathname = folder;
+            mPathname += ("/");
+            mPathname += (filename);
+            mPathname += (".ids");
+            mKeys = new Dictionary<ObjectId, OffsetAndLength>();
+            Load();
+        }
+
+        /// <summary>
+        /// Adds / updates the input objectId into memory.
+        /// </summary>
+        /// <param name="objectId"></param>
+        /// <param name="dataFileOffset"></param>
+        /// <param name="dataSizeBytes"></param>
+        public void Update(ObjectId objectId, int dataFileOffset, int dataSizeBytes)
+        {
+            var offsetAndLength = new OffsetAndLength();
+            offsetAndLength.fileOffset = dataFileOffset;
+            offsetAndLength.sizeBytes = dataSizeBytes;
+            if (mKeys.ContainsKey(objectId))
+            {
+                mKeys[objectId] = offsetAndLength;
+            }
+            else
+            {
+                mKeys.Add(objectId, offsetAndLength);
+            }
+        }
+
+        /// <summary>
+        /// Looks up the input objectId in memory.
+        /// </summary>
+        /// <param name="objectId"></param>
+        /// <param name="dataFileOffset"></param>
+        /// <param name="dateSizeBytes"></param>
+        /// <returns></returns>
+        public bool Get(ObjectId objectId, ref int dataFileOffset, ref int dateSizeBytes)
+        {
+            if (mKeys.ContainsKey(objectId))
+            {
+                OffsetAndLength offsetAndLength = mKeys[objectId];
+                dataFileOffset = offsetAndLength.fileOffset;
+                dateSizeBytes = offsetAndLength.sizeBytes;
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Writes the Key File to persistent storage.
+        /// </summary>
+        public void Save()
+        {
+            var outputStream = new StreamWriter(mPathname);
+            foreach (KeyValuePair<ObjectId, OffsetAndLength> entry in mKeys)
+            {
+                OffsetAndLength offsetAndLength = entry.Value;
+                String outputline = entry.Key + " " +
+                offsetAndLength.fileOffset + " " +
+                offsetAndLength.sizeBytes + "\r\n";
+                outputStream.Write(outputline);
+            }
+            outputStream.Close();
+        }
+
+        /// <summary>
+        /// Loads the Key File into memory.
+        /// </summary>
+        private void Load()
+        {
+            var classNameMap = new Dictionary<String, int>();
+
+            if (File.Exists(mPathname))
+            {
+                var fileReader = new StreamReader(mPathname);
+                var inputLine = fileReader.ReadLine();
+                while (inputLine != null)
+                {
+                    var column = inputLine.Split(' ');
+                    var objectId = new ObjectId(Convert.ToInt32(column[0]));
+                    var offsetAndLength = new OffsetAndLength();
+                    offsetAndLength.fileOffset = Convert.ToInt32(column[1]);
+                    offsetAndLength.sizeBytes = Convert.ToInt32(column[2]);
+                    mKeys.Add(objectId, offsetAndLength);
+                    inputLine = fileReader.ReadLine();
+                }
+
+                fileReader.Close();
+            }
+        }
+
+        class OffsetAndLength
+        {
+            public int fileOffset; // Offset into data file 
+            public int sizeBytes; // Number of bytes in record 
+        }
+}
+}
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
---
+++ DiaryTest/Persistence/KeyFileTest.cs
@@ -0,0 +1,46 @@
+﻿using Diary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DiaryTest
+{
+    /// <summary>
+    /// Tests the KeyFile class.
+    /// </summary>
+    [TestClass]
+    public class KeyFileTest
+    {
+        /// <summary>
+        /// Tests the creation of key files.
+        /// </summary>
+        [TestMethod]
+        public void TestKeyFileCreation()
+        {
+            var keyFile = new KeyFile("C:/Persistence", "Test");
+
+            var objectIdA = new ObjectId();
+            var objectIdB = new ObjectId();
+            var objectIdC = new ObjectId();
+
+            keyFile.Update(objectIdA, 100, 1000);
+            keyFile.Update(objectIdB, 200, 2000);
+            keyFile.Update(objectIdC, 300, 3000);
+
+            int offset = 0;
+            int dataSizeBytes = 0;
+
+            Assert.IsTrue(keyFile.Get(objectIdA, ref offset, ref dataSizeBytes));
+            Assert.IsTrue(offset == 100);
+            Assert.IsTrue(dataSizeBytes == 1000);
+
+            Assert.IsTrue(keyFile.Get(objectIdB, ref offset, ref dataSizeBytes));
+            Assert.IsTrue(offset == 200);
+            Assert.IsTrue(dataSizeBytes == 2000);
+
+            Assert.IsTrue(keyFile.Get(objectIdC, ref offset, ref dataSizeBytes));
+            Assert.IsTrue(offset == 300);
+            Assert.IsTrue(dataSizeBytes == 3000);
+
+            keyFile.Save();
+        }
+    }
+}
