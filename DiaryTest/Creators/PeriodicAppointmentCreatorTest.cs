71fb917482d923389467796c665240cec2f9f370
Cherié Warren <CheriePhi@msn.com>
2017-06-01 15:19:20 -0700

    Reworked PeriodicAppointmentCreator tests to match other creator test patterns

--- DiaryTest/Creators/PeriodicAppointmentCreatorTest.cs
+++ DiaryTest/Creators/PeriodicAppointmentCreatorTest.cs
@@ -1,6 +1,5 @@
 ﻿using Diary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 
 namespace DiaryTest.Creators
 {
@@ -36,65 +35,62 @@ namespace DiaryTest.Creators
         #endregion
 
         /// <summary>
-        /// Tests the PeriodicAppointment accessors through the PeriodicAppointmentCreator constructor.
-        /// </summary>
-        [TestMethod]
-        public void PeriodicAppointmentCreatorConstructorTest()
-        {
-            // Wrap the creator in a using block so its resources will get released when the program no longer needs it.
-            using (var creator = new PeriodicAppointmentCreator())
-            {
-                var builder = new PeriodicAppointmentBuilder();
-                builder.SetCreator(creator);
-                builder.SetOccurs(new Diary.DateTime(new Date(15, Date.Month.FEBRUARY, 2008), 15, 30));
-                builder.SetLabel("Dr Apt");
-                builder.SetDetails("At the library");
-
-                Helper.AssertAreEqual(builder, (Appointment)builder.Build(), "");
-            }
-        }
-
-        /// <summary>
-        /// Tests persistence via the PeriodicAppointmentCreator.
+        /// Tests the PeriodicAppointment accessors and persistence through the PeriodicAppointmentCreator.
         /// </summary>
         [TestMethod]
         public void SaveAndLoadTest()
         {
-            var appointments = new PeriodicAppointment[3];
+            var builders = new PeriodicAppointmentBuilder[3]
+            {
+                new PeriodicAppointmentBuilder(),
+                new PeriodicAppointmentBuilder(),
+                new PeriodicAppointmentBuilder()
+            };
 
             using (var creator = new PeriodicAppointmentCreator())
             {
-                var yogaBuilder = new PeriodicAppointmentBuilder();
-                yogaBuilder.SetCreator(creator);
-                yogaBuilder.SetOccurs(new Diary.DateTime(new Date(1, Date.Month.FEBRUARY, 2017), 6, 30));
-                yogaBuilder.SetDurationMinutes(45);
-                yogaBuilder.SetLabel("Yoga");
-                yogaBuilder.SetDetails("Downward Dog");
-                yogaBuilder.SetPeriodHours(24);
-                yogaBuilder.SetNotToExceedDateTime(new Diary.DateTime(new Date(1, Date.Month.MARCH, 2017), 6, 30));
-                appointments[0] = (PeriodicAppointment)yogaBuilder.Build();
+                builders[0].SetOccurs(new Diary.DateTime(new Date(1, Date.Month.FEBRUARY, 2017), 6, 30));
+                builders[0].SetDurationMinutes(45);
+                builders[0].SetLabel("Yoga");
+                builders[0].SetDetails("Downward Dog");
+                builders[0].SetPeriodHours(24);
+                builders[0].SetNotToExceedDateTime(new Diary.DateTime(new Date(1, Date.Month.MARCH, 2017), 6, 30));
 
-                var doctorBuilder = new PeriodicAppointmentBuilder();
-                doctorBuilder.SetCreator(creator);
-                doctorBuilder.SetOccurs(new Diary.DateTime(new Date(2, Date.Month.MARCH, 2017), 8, 20));
-                doctorBuilder.SetDurationMinutes(30);
-                doctorBuilder.SetLabel("Ear Doctor");
-                doctorBuilder.SetDetails("Annual Cleaning");
-                doctorBuilder.SetPeriodHours(24 * 365);
-                doctorBuilder.SetNotToExceedDateTime(new Diary.DateTime(new Date(1, Date.Month.MARCH, 2022), 8, 20));
-                appointments[1] = (PeriodicAppointment)doctorBuilder.Build();
+                builders[1].SetOccurs(new Diary.DateTime(new Date(2, Date.Month.MARCH, 2017), 8, 20));
+                builders[1].SetDurationMinutes(30);
+                builders[1].SetLabel("Ear Doctor");
+                builders[1].SetDetails("Annual Cleaning");
+                builders[1].SetPeriodHours(24 * 365);
+                builders[1].SetNotToExceedDateTime(new Diary.DateTime(new Date(1, Date.Month.MARCH, 2022), 8, 20));
+ 
+                builders[2].SetOccurs(new Diary.DateTime(new Date(7, Date.Month.APRIL, 2017), 12, 00));
+                builders[2].SetDurationMinutes(25);
+                builders[2].SetLabel("School");
+                builders[2].SetDetails("5th Grade Arithmetic");
+                builders[2].SetPeriodHours(24);
+                builders[2].SetNotToExceedDateTime(new Diary.DateTime(new Date(12, Date.Month.APRIL, 2017), 12, 00));
 
-                var teacherBuilder = new PeriodicAppointmentBuilder();
-                teacherBuilder.SetCreator(creator);
-                teacherBuilder.SetOccurs(new Diary.DateTime(new Date(7, Date.Month.APRIL, 2017), 12, 00));
-                teacherBuilder.SetDurationMinutes(25);
-                teacherBuilder.SetLabel("School");
-                teacherBuilder.SetDetails("5th Grade Arithmetic");
-                teacherBuilder.SetPeriodHours(24);
-                teacherBuilder.SetNotToExceedDateTime(new Diary.DateTime(new Date(12, Date.Month.APRIL, 2017), 12, 00));
-                appointments[2] = (PeriodicAppointment)teacherBuilder.Build();
+                foreach (var builder in builders)
+                {
+                    builder.SetCreator(creator);
+                    var periodicAppointment = (PeriodicAppointment)builder.Build();
+                    builder.SetObjectId(periodicAppointment.GetObjectId());
+
+                    Helper.AssertAreEqual(builder, periodicAppointment, "Original");
+                }
 
                 creator.Save();
+            }
+
+            using (var creator = new PeriodicAppointmentCreator())  // Re-open the files.
+            {
+                foreach (var builder in builders)
+                {
+                    var objectId = builder.GetObjectId();
+                    var savedPeriodicAppointment = (PeriodicAppointment)creator.Create(objectId);
+
+                    Helper.AssertAreEqual(builder, savedPeriodicAppointment, "Saved");
+                }
             }
         }
     }
